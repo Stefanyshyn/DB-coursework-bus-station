cdcac4e13315a7d041432498b5aebae7f38443da
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2019-11-15 14:38:09 +0200

    feat: added user and profile models

---
+++ BusStation/BusStation/Models/Profile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusStation.Models
+{
+    class Profile
+    {
+        public string firstName{ get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ BusStation/BusStation/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusStation.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public Profile profile { get; set; }
+        public DateTime CreateAt { get; set; }
+
+    }
+}
