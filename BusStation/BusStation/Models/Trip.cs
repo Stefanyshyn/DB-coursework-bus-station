52cc08ac61320540e6d68a14e7c3d8f5ef3fee10
Ivan Stefanyshyn <ivanstefanushun@gmail.com>
2019-11-18 17:43:24 +0200

    feat: added trip model

---
+++ BusStation/BusStation/Models/Trip.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusStation.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+
+    }
+}
